e9c1a66da04aeebbd25b23b1f18e94ec4e9fa8a1
Jeferson Luiz Alves de Souza <46039615+jeffalves33@users.noreply.github.com>
2021-08-27 16:24:42 -0300

    Create scripts.js

---
+++ cronometro/scripts.js
@@ -0,0 +1,39 @@
+var [hour,minutes,seconds] = [0,0,0];
+const tempo = 1000; //1 second
+var cron;
+
+function start(){ cron = setInterval(timer,tempo);}
+
+function pause(){clearInterval(cron);}
+
+function stop(){
+    const format = (hour < 10 ? '0' + hour : hour) + ":" + 
+                 (minutes < 10 ? '0' + minutes : minutes) + ":" +
+                 (seconds < 10 ? '0' + seconds : seconds);
+    document.getElementById('result_antegior').innerHTML = "Último resultado: " + format;
+    clearInterval(cron);
+    [hour,minutes,seconds] = [0,0,0];
+    document.getElementById('h1-Timer').innerText = "00:00:00";
+}
+
+function timer(){
+    seconds++;
+    if(seconds == 60){
+        seconds = 0;
+        minutes ++;
+        
+        if(minutes == 60){
+            minutes = 0;
+            hour++;
+
+            if(hour == 23){
+                hour = 0;
+            }
+        }
+    }
+    
+    const format = (hour < 10 ? '0' + hour : hour) + ":" + 
+                 (minutes < 10 ? '0' + minutes : minutes) + ":" +
+                 (seconds < 10 ? '0' + seconds : seconds);
+    document.getElementById('h1-Timer').innerText = format;
+}
